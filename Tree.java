3b946bbefe6a17947ee23caa078e83bf0350ca87
vijay-aruchamy <vijayaruchamy2003@gmail.com>
2023-10-09 19:29:33 +0530

    first commit

---
+++ Tree.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Tree
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+
+    }
+}
